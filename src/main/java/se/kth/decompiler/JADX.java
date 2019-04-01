1534d45a3874ec091724913ae65dbe1ac214d499
nharrand <nicolas.harrand@gmail.com>
2019-04-01 16:30:16 +0200

    fix JADX boiler plates

--- src/main/java/se/kth/decompiler/JADX.java
+++ src/main/java/se/kth/decompiler/JADX.java
@@ -1,5 +1,6 @@
 package se.kth.decompiler;
 
+import org.apache.commons.io.FileUtils;
 import se.kth.Decompiler;
 
 
@@ -8,18 +9,22 @@ import java.io.File;
 public class JADX implements Decompiler {
 
     @Override
-    public boolean decompile(File in, File outDir, String cl) {
+    public boolean decompile(File in, File outputDir, String cl) {
 
+        File inBase = new File(in.getAbsolutePath().replace(cl + ".class", ""));
         try {
             ProcessBuilder pb = new ProcessBuilder(
-                    "./jadx", // build-in script to run the decompiler
+                    "./lib/jadx-0.9.0/bin/jadx", // build-in script to run the decompiler
                     "-r", // do not decode resources
-                    "-d", outDir.getAbsolutePath(), // output directory
+                    "-d", "tmp", // output directory
                     in.getAbsolutePath() // input file (.dex, .apk, .jar or .class)
-            );
-            pb.directory(new File("lib/jadx-0.9.0/bin"));
+            ).inheritIO();
+            //pb.directory(new File("lib/jadx-0.9.0/bin"));
+
             Process p = pb.start();
             int r =  p.waitFor();
+            FileUtils.moveFile(new File("tmp/sources/" + cl + ".java"), new File(outputDir, cl + ".java"));
+            if(new File("tmp/sources").exists()) FileUtils.deleteQuietly(new File("tmp/sources"));
             return r==0;
         } catch (Exception e) {
             e.printStackTrace();
