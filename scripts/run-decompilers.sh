ff3e99516b14d542b5ac3faa0a798a5ef6926070
nharrand <nicolas.harrand@gmail.com>
2019-04-01 16:03:39 +0200

    update run script

--- scripts/run-decompilers.sh
+++ scripts/run-decompilers.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-PROJECTS=""
+PROJECTS="Bukkit commons-collections commons-lang mimecraft commons-codec commons-imaging DiskLruCache jsoup scribejava joda-time"
+#PROJECTS="joda-time mimecraft"
+#PROJECTS="Bukkit"
 
-DECOMPILERS=""
+#DECOMPILERS="Procyon-0.5.34 Jode-1.1.2-pre1 CFR-0.141 Fernflower-2.5.0.Final Krakatau JD-GUI-1.4.1 Dava-3.3.0"
+DECOMPILERS="JADX-0.9.0"
 
-CLASSPATH=""
+CLASSPATH="target/decompiler-cmp-1.0-SNAPSHOT-jar-with-dependencies.jar:lib/jd-gui-1.4.1.jar:lib/jode-1.1.2-pre1.jar:lib/sootclasses-trunk-jar-with-dependencies.jar"
 
-LOG_DIR=""
+LOG_DIR="log"
+
+REPOS_DIR="repos"
 
 echo "Start decompiler run"
 for p in $PROJECTS
@@ -15,6 +20,6 @@ do
 	for d in $DECOMPILERS
 	do
 		echo "Start decompiler run on $p with $d"
-		java -cp $CLASSPATH se.kth.DecompilerComparator -p $p -d $d 1> ${LOG_DIR}/${p}-${d}.log 2> ${LOG_DIR}/${p}-${d}.log
+		java -cp $CLASSPATH se.kth.DecompilerComparator -p $REPOS_DIR/$p -d $d 1> ${LOG_DIR}/${p}-${d}.log 2> ${LOG_DIR}/${p}-${d}.log
 	done
 done
