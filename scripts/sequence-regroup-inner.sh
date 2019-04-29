2514e2bd68c0ae923629c39475ef3531b99ca3fd
nharrand <nicolas.harrand@gmail.com>
2019-04-29 20:39:27 +0200

    add byte code sequencing scripts

---
+++ scripts/sequence-regroup-inner.sh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+
+regroupInnerClasses() {
+	#get direct children
+	local main=$1
+	#echo "main: ${main}"
+	local mainEscape="s/"$(echo "./${main}\$" | sed 's/\//\\\//g' | sed 's/\$/\\$/g')"//"
+	#echo "mainEscape: ${mainEscape}"
+	local prefix="./"$(echo $main | sed 's/\$/\\$/g')"\\\$*"
+	#echo "prefix: $prefix"
+	#find . -wholename "$prefix"
+	for child in $(find . -wholename $(echo "$prefix") | sed $mainEscape | grep -v "\\$")
+	do
+		echo "$child"
+		local tmp=$main
+		local param=$(echo "${tmp}\$$child")
+		#echo "param: $param"
+		regroupInnerClasses $param
+		echo "inner_start" >> $tmp
+		cat ${param} >> $tmp
+		echo "inner_end" >> $tmp
+	done
+	#echo "done"
+}
+
+ROOT=$(pwd)
+OUTPUTDIR="sequences"
+COMPILERS=('javac')
+REPOS=('commons-lang' 'Bukkit' 'commons-codec' 'commons-collections' 'commons-imaging' 'dctest' 'DiskLruCache' 'javapoet' 'joda-time' 'jsoup' 'junit4' 'mimecraft' 'scribejava' 'spark')
+
+#repo="dctest"
+#for repo in $(ls repos)
+for repo in $REPOS
+do
+	echo "REPO: $repo"
+	for c in $COMPILERS
+	do
+		cd $ROOT
+		cd sequences/$repo/$c
+		echo "COMPILER: $c"
+		#for mainClass in $(find . -type f | grep "\\$" | cut -d '$' -f1 | sort | uniq | sed 's/.\/sequences\/'"$repo"'\/'"$c"'//')
+		for mainClass in $(find . -type f | grep "\\$" | cut -d '$' -f1 | sort | uniq | sed 's/.\///')
+		do
+			#echo "mainClass: "$(echo $mainClass | sed 's/\$/\\$/g')
+			regroupInnerClasses $mainClass
+		done
+	done
+	COMPILERS=('javac' 'eclipse')
+	
+done
+
+cd $ROOT
---
+++ scripts/sequence.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ROOT=$(pwd)
+OUTPUTDIR="sequences"
+COMPILERS="javac"
+REPOS="commons-lang Bukkit commons-codec commons-collections commons-imaging dctest DiskLruCache javapoet joda-time jsoup junit4 mimecraft scribejava spark"
+
+#repo="dctest"
+#for repo in $(ls repos)
+for repo in $REPOS
+do
+	echo "REPO: $repo"
+	for c in $COMPILERS
+	do
+		cd $ROOT
+		cd repos/$repo
+		echo "COMPILER: $c"
+		mvn clean compile -Dmaven.compiler.compilerId=$c
+		cd target/classes
+		for cl in $(find . -name "*.class" | sed 's/.\///')
+		do
+			CL_DIR=$(dirname $cl)
+			mkdir -p $ROOT/$OUTPUTDIR/$repo/$c/$CL_DIR
+			CL_DOT=$(echo $cl | sed 's/\//./g' | sed 's/.class//')
+			javap -v -c $CL_DOT 2> /dev/null | grep -E "^[[:space:]]+[0-9]+:" | sed 's/ 0:/ 0: method\n         0:/' | cut -d ':' -f2 | cut -d ' ' -f2 > $ROOT/$OUTPUTDIR/$repo/$c/$(echo $cl | sed 's/.class//')
+		done
+	done
+	COMPILERS="javac eclipse"
+	
+done
+
+cd $ROOT
